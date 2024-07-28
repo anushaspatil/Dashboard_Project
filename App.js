8a492b49af6999f2824838fb7d562539efc251d8
anushaspatil <135057729+anushaspatil@users.noreply.github.com>
2024-07-28 14:46:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+const optionMenu = document.querySelector(".moremenu")
+const asideLeft = document.querySelector("aside.left")
+
+
+optionMenu.addEventListener("click", ()=>{
+  asideLeft.style.display = "block";
+})
